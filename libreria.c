4210b3bdc26547cdcd23e7adb1758d2d01646f19
franpassa <francopassarelli94@gmail.com>
2019-11-19 21:16:32 -0300

    Arreglo libreria.h y libreria.c. Cambio leve en los struct

--- libreria.c
+++ libreria.c
@@ -1,50 +1,58 @@
-/*Estructuras*/
+#include "libreria.h"
 
-//Escenario
+anfitrion* inicializarAnfitrion(char* nombre,int energia,float procesamiento,nodo_recuerdo* Recuerdos){
+	anfitrion* nuevoAnfitrion = malloc(sizeof(anfitrion));
+	nuevoAnfitrion->nombre = strdup(nombre);
+	nuevoAnfitrion->energia = energia;
+	nuevoAnfitrion->procesamiento = procesamiento;
+	nuevoAnfitrion->recuerdos = Recuerdos;
+	return nuevoAnfitrion;
+}
 
-typedef struct{
-    char* nombre;
-    char* categoria;
-}escenario;
+recuerdo* inicializarRecuerdo(char* descripcion, escenario Escenario){
+    recuerdo* nuevoRecuerdo = malloc(sizeof(recuerdo));
+    nuevoRecuerdo->descripcion = strdup(descripcion);
+    nuevoRecuerdo->Escenario = Escenario;
+    return nuevoRecuerdo;
+}
 
-//Recuerdos
+escenario inicializarEscenario(char* nombre,char* categoria){
+    escenario nuevoEscenario;
+    nuevoEscenario.nombre = nombre;
+    nuevoEscenario.categoria = categoria;
+    return nuevoEscenario;
+}
 
-typedef struct{
-    char* descripcion;
-    escenario Escenario;
-}recuerdo;
+nodo_recuerdo* agregarRecuerdoALista(recuerdo* Recuerdo,nodo_recuerdo* Lista){
+    nodo_recuerdo* nuevoNodo = (nodo_recuerdo*)malloc(sizeof(nodo_recuerdo));
+    nuevoNodo->Recuerdo = *Recuerdo;
+    nuevoNodo->siguiente = Lista;
+    return nuevoNodo;
+}
 
-typedef struct{
-    recuerdo Recuerdo;
-    struct nodo_recuerdo* siguiente;
-}nodo_recuerdo;
+nodo_anfitrion* agregarAnfitrionALista(anfitrion* Anfitrion, nodo_anfitrion* Lista)
+{
+    nodo_anfitrion* nuevoNodo = (nodo_anfitrion*)malloc(sizeof(nodo_anfitrion));
+    nuevoNodo->anfitrion = *Anfitrion;
+    nuevoNodo->siguiente = Lista;
+    return nuevoNodo;
+}
 
-//Anfitriones
+void mostrarRecuerdos(nodo_recuerdo* Lista){
+    while(Lista!=NULL){
+        printf("\tRecuerdo: %s\n",Lista->Recuerdo.descripcion);
+        printf("\tNombre del escenario: %s\n",Lista->Recuerdo.Escenario.nombre);
+        printf("\tCategoria del escenario: %s\n",Lista->Recuerdo.Escenario.categoria);
+        Lista = Lista->siguiente;
+    }
+}
 
-typedef struct{
-	char* nombre;
-	int energia;
-	float procesamiento;
-	nodo_recuerdo* recuerdos;
-}anfitrion;
-
-typedef struct{
-	anfitrion anfitrion;
-	struct nodo_anfitrion* siguiente;
-}nodo_anfitrion;
-
-/*Funciones*/
-//Funciones para inicializar las estructuras
-
-anfitrion* inicializarAnfitrion(char* nombre,int energia,float procesamiento,nodo_recuerdo* Recuerdos);
-recuerdo* inicializarRecuerdo(char* descripcion, escenario Escenario);
-escenario inicializarEscenario(char* nombre, char* categoria);
-
-//Funciones para agregar elementos a las listas
-
-nodo_anfitrion* agregarAnfitrionALista(anfitrion* Anfitrion, nodo_anfitrion* Lista);
-nodo_recuerdo* agregarRecuerdoALista(recuerdo* Recuerdo, nodo_recuerdo* Lista);
-
-//Funciones para mostrar
-
-void mostrarListaAnfitriones(nodo_anfitrion* Lista);
+void mostrarListaAnfitriones(nodo_anfitrion* Lista){
+	while(Lista!=NULL){
+		printf("Nombre: %s\n",Lista->anfitrion.nombre);
+		printf("Energia: %d\n",Lista->anfitrion.energia);
+		printf("Procesamiento: %f\n",Lista->anfitrion.procesamiento);
+		mostrarRecuerdos(Lista->anfitrion.recuerdos);
+		Lista = Lista->siguiente;
+	}
+}
--- libreria.h
+++ libreria.h
@@ -1,61 +1,54 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "libreria.c"
 
-anfitrion* inicializarAnfitrion(char* nombre,int energia,float procesamiento,nodo_recuerdo* Recuerdos){
-	anfitrion* nuevoAnfitrion = malloc(sizeof(anfitrion));
-	nuevoAnfitrion->nombre = strdup(nombre);
-	nuevoAnfitrion->energia = energia;
-	nuevoAnfitrion->procesamiento = procesamiento;
-	nuevoAnfitrion->recuerdos = Recuerdos;
-	return nuevoAnfitrion;
-}
+/*Estructuras*/
 
-recuerdo* inicializarRecuerdo(char* descripcion, escenario Escenario){
-    recuerdo* nuevoRecuerdo = malloc(sizeof(recuerdo));
-    nuevoRecuerdo->descripcion = strdup(descripcion);
-    nuevoRecuerdo->Escenario = Escenario;
-    return nuevoRecuerdo;
-}
+//Escenario
 
-escenario inicializarEscenario(char* nombre,char* categoria){
-    escenario nuevoEscenario;
-    nuevoEscenario.nombre = nombre;
-    nuevoEscenario.categoria = categoria;
-    return nuevoEscenario;
-}
+typedef struct escenario{
+    char* nombre;
+    char* categoria;
+}escenario;
 
-nodo_recuerdo* agregarRecuerdoALista(recuerdo* Recuerdo,nodo_recuerdo* Lista){
-    nodo_recuerdo* nuevoNodo = (nodo_recuerdo*)malloc(sizeof(nodo_recuerdo));
-    nuevoNodo->Recuerdo = *Recuerdo;
-    nuevoNodo->siguiente = Lista;
-    return nuevoNodo;
-}
+//Recuerdos
 
-nodo_anfitrion* agregarAnfitrionALista(anfitrion* Anfitrion, nodo_anfitrion* Lista)
-{
-    nodo_anfitrion* nuevoNodo = (nodo_anfitrion*)malloc(sizeof(nodo_anfitrion));
-    nuevoNodo->anfitrion = *Anfitrion;
-    nuevoNodo->siguiente = Lista;
-    return nuevoNodo;
-}
+typedef struct recuerdo{
+    char* descripcion;
+    escenario Escenario;
+}recuerdo;
 
-void mostrarRecuerdos(nodo_recuerdo* Lista){
-    while(Lista!=NULL){
-        printf("\tRecuerdo: %s\n",Lista->Recuerdo.descripcion);
-        printf("\tNombre del escenario: %s\n",Lista->Recuerdo.Escenario.nombre);
-        printf("\tCategoria del escenario: %s\n",Lista->Recuerdo.Escenario.categoria);
-        Lista = Lista->siguiente;
-    }
-}
+typedef struct nodo_recuerdo{
+    recuerdo Recuerdo;
+    struct nodo_recuerdo* siguiente;
+}nodo_recuerdo;
 
-void mostrarListaAnfitriones(nodo_anfitrion* Lista){
-	while(Lista!=NULL){
-		printf("Nombre: %s\n",Lista->anfitrion.nombre);
-		printf("Energia: %d\n",Lista->anfitrion.energia);
-		printf("Procesamiento: %f\n",Lista->anfitrion.procesamiento);
-		mostrarRecuerdos(Lista->anfitrion.recuerdos);
-		Lista = Lista->siguiente;
-	}
-}
+//Anfitriones
+
+typedef struct anfitrion{
+	char* nombre;
+	int energia;
+	float procesamiento;
+	nodo_recuerdo* recuerdos;
+}anfitrion;
+
+typedef struct nodo_anfitrion{
+	anfitrion anfitrion;
+	struct nodo_anfitrion* siguiente;
+}nodo_anfitrion;
+
+/*Funciones*/
+//Funciones para inicializar las estructuras
+
+anfitrion* inicializarAnfitrion(char* nombre,int energia,float procesamiento,nodo_recuerdo* Recuerdos);
+recuerdo* inicializarRecuerdo(char* descripcion, escenario Escenario);
+escenario inicializarEscenario(char* nombre, char* categoria);
+
+//Funciones para agregar elementos a las listas
+
+nodo_anfitrion* agregarAnfitrionALista(anfitrion* Anfitrion, nodo_anfitrion* Lista);
+nodo_recuerdo* agregarRecuerdoALista(recuerdo* Recuerdo, nodo_recuerdo* Lista);
+
+//Funciones para mostrar
+
+void mostrarListaAnfitriones(nodo_anfitrion* Lista);
